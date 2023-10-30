d3992f328564a6e301a0a52cace900826e1e310c
Aron Gyulai <arongyf@gmail.com>
2023-10-30 10:43:50 +0100

    added files

--- src/FemtonSpel/Bricks.java
+++ src/FemtonSpel/Bricks.java
@@ -5,10 +5,40 @@ import javax.swing.*;
 public class Bricks {
 
     JPanel brick = new JPanel();
-    boolean inPosition = true;
+    private boolean inPosition = true;
+    private int position;
 
     public Bricks(){
 
+    }
+
+    public Bricks(JPanel brick, int position) {
+        this.brick = brick;
+        this.position = position;
+    }
+
+    public JPanel getBrick() {
+        return brick;
+    }
+
+    public void setBrick(JPanel brick) {
+        this.brick = brick;
+    }
+
+    public boolean isInPosition() {
+        return inPosition;
+    }
+
+    public void setInPosition(boolean inPosition) {
+        this.inPosition = inPosition;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
     }
 
     public void changePosition(){
---
+++ src/FemtonSpel/FemtonSetup.java
@@ -0,0 +1,4 @@
+package FemtonSpel;
+
+public class FemtonSetup {
+}
